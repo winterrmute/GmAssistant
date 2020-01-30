0a5ee7d9f2b47f37b8dc3bd881648f11e4664f23
Kamil Kosinski <hkkosinski@gmail.com>
2020-01-30 20:32:24 +0100

    WIP: browsing files works now in both directions

--- app/src/main/java/com/wintermute/gmassistant/adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/wintermute/gmassistant/adapters/ViewPagerAdapter.java
@@ -16,7 +16,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ViewPagerAdapter extends RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>
@@ -60,36 +59,26 @@ public class ViewPagerAdapter extends RecyclerView.Adapter<ViewPagerAdapter.View
 
     private void browseDirectory(FileBrowserService fbs, FileElement item)
     {
-        Map<String, List<FileElement>> files = fbs.getFiles(item);
-        List<FileElement> newContent = files.values().stream().flatMap(Collection::stream).collect(Collectors.toList());
-        String parent;
-        String path = item.getPath();
+        List<FileElement> newContent;
+        String newPath = item.getPath();
 
-
-        if (item.isRoot())
-        {
+        if (item.isRoot()) {
             rootPath = item.getPath();
         }
-        FileElement goToParent = new FileElement(PREVIOUS_DIRECTORY, item.getPath(), false);
 
-        if (PREVIOUS_DIRECTORY.equals(item.getName())) {
-            if (item.getPath().equals(rootPath)) {
-                newContent = rootElements;
-                updateListElements(newContent);
-            } else {
-                goToParent = new FileElement(PREVIOUS_DIRECTORY, item.getPath().substring(item.getPath().lastIndexOf(0, '/')) , false);
-                newContent.add(0, goToParent);
-                updateListElements(newContent);
-            }
-        } else {
-            newContent.add(0, goToParent);
-            updateListElements(newContent);
+        if (PREVIOUS_DIRECTORY.equals(item.getName()))
+        {
+            newPath = item.getPath().substring(0, item.getPath().lastIndexOf('/'));
         }
+        FileElement goToParent = new FileElement(PREVIOUS_DIRECTORY, newPath, false);
 
-    }
-
-    private void handlePath(){
-        
+        if (item.getPath().equals(rootPath.substring(0, rootPath.lastIndexOf('/')))) {
+            newContent = rootElements;
+        } else {
+            newContent = fbs.getFiles(item).values().stream().flatMap(Collection::stream).collect(Collectors.toList());
+            newContent.add(0, goToParent);
+        }
+        updateListElements(newContent);
     }
 
     private void updateListElements(List<FileElement> newContent)
