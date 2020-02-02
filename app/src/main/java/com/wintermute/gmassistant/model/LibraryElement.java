9c3a90056564cccd1f5fb83e4ccd96fa7e514f21
Kamil Kosinski <winterrmut3@gmail.com>
2020-02-02 19:23:01 +0100

    small refactor

---
+++ app/src/main/java/com/wintermute/gmassistant/model/LibraryElement.java
@@ -0,0 +1,54 @@
+package com.wintermute.gmassistant.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Represents file collected by {@link com.wintermute.gmassistant.services.FileBrowserService}
+ */
+@Data
+@AllArgsConstructor
+public class LibraryElement implements Parcelable
+{
+    private String name;
+    private String path;
+    private boolean isRoot;
+
+    public static final Creator<LibraryElement> CREATOR = new Creator<LibraryElement>()
+    {
+        @Override
+        public LibraryElement createFromParcel(Parcel in)
+        {
+            return new LibraryElement(in);
+        }
+
+        @Override
+        public LibraryElement[] newArray(int size)
+        {
+            return new LibraryElement[size];
+        }
+    };
+
+    private LibraryElement(Parcel in)
+    {
+        name = in.readString();
+        path = in.readString();
+        isRoot = in.readByte() != 0;
+    }
+
+    @Override
+    public int describeContents()
+    {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags)
+    {
+        dest.writeString(name);
+        dest.writeString(path);
+        dest.writeByte((byte) (isRoot ? 1 : 0));
+    }
+}
